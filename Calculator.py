fe4efb2ba0735cf57825493abca0f36195118632
GAMER <michaelissao923@gmail.com>
2025-06-22 15:18:50 -0300

    First calculator

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,16 @@
-def soma(a, b):
-    return a + b
+def soma(number1, number2):
+    return number1 + number2
 
-def subtrai(a, b):
-    return a - b
+def subtrai(number1, number2):
+    return number1 - number2
 
-def multiplica(a, b):
-    return a * b
+def multiplica(number1, number2):
+    return number1 * number2
 
-def divide(a, b):
+def divide(number1, number2):
     if b == 0:
         return "Erro: divisão por zero"
-    return a / b
+    return number1 / number2
 
 print("Calculadora Simples")
 print("Escolha a operação:")
@@ -21,16 +21,22 @@ print("4 - Divisão")
 
 opcao = input("Digite a opção (1/2/3/4): ")
 
-a = float(input("Digite o primeiro número: "))
-b = float(input("Digite o segundo número: "))
+number1 = float(input("Digite o primeiro número: "))
+number2 = float(input("Digite o segundo número: "))
 
 if opcao == '1':
-    print("Resultado:", soma(a, b))
+    print("Resultado:", soma(number1, number2))
+    print("----------------------------------------")
 elif opcao == '2':
-    print("Resultado:", subtrai(a, b))
+    print("Resultado:", subtrai(number1, number2))
+    print("----------------------------------------")
 elif opcao == '3':
-    print("Resultado:", multiplica(a, b))
+    print("Resultado:", multiplica(number1, number2))
+    print("----------------------------------------")
 elif opcao == '4':
-    print("Resultado:", divide(a, b))
+    print("Resultado:", divide(number1, number2))
+    print("----------------------------------------")
 else:
     print("Opção inválida")
+    print("----------------------------------------")
+    
